fa864da76f16b4f4c40cbe6b8121596d796acdb8
Guru Darji <gurudarji@ymail.com>
2022-03-08 15:29:02 -0500

    added display for UV index

--- scripts.js
+++ scripts.js
@@ -100,7 +100,28 @@ var displayWeather = function(weather, searchCity){
     //console.log(lon);
 }
  
+var displayUvIndex = function(index){
+    var uvIndexEl = document.createElement("div");
+    uvIndexEl.textContent = "UV Index: "
+    uvIndexEl.classList = "list-group-item"
 
+    uvIndexValue = document.createElement("span")
+    uvIndexValue.textContent = index.value
+
+    if(index.value <=2){
+        uvIndexValue.classList = "favorable"
+    }else if(index.value >2 && index.value<=8){
+        uvIndexValue.classList = "moderate "
+    }
+    else if(index.value >8){
+        uvIndexValue.classList = "severe"
+    };
+
+    uvIndexEl.appendChild(uvIndexValue);
+
+    //append index to current weather
+    weatherContainerEl.appendChild(uvIndexEl);
+}
 
 
 
