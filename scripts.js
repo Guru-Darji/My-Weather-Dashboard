62eeb04bd7a2e3de33f053b7bb1afcba5ebfeab9
Guru Darji <gurudarji@ymail.com>
2022-03-08 14:33:10 -0500

    made form submission handler

--- scripts.js
+++ scripts.js
@@ -9,6 +9,35 @@ var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
 
+var formSumbitHandler = function(event){
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else{
+        alert("Please enter a City:");
+    }
+    saveSearch();
+    pastSearch(city);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 cityFormEl.addEventListener("submit", formSumbitHandler);
 pastSearchButtonEl.addEventListener("click", pastSearchHandler);
